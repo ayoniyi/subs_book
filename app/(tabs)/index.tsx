532dd753f39c9ecd8abf8ee27a399647a7211132
ayoniyi <ayo.a.niyi@gmail.com>
2026-04-22 16:19:58 +0100

    add: font implementation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,6 +16,7 @@
 
 import "@/global.css";
 import { Link } from "expo-router";
+
 import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
@@ -25,34 +26,32 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5  justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className=" font-sans-extrabold text-primary text-5xl">Home</Text>
       <Link
         href={"/onboarding"}
-        className="mt-4 rounded bg-primary p-4 text-white"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
       >
         Go to Onboarding
       </Link>
       <Link
         href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary p-4 text-white"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
       >
         Go to SignIn
       </Link>
       <Link
         href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary p-4 text-white"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
       >
         Go to SignUp
       </Link>
 
-      <Link
+      {/* <Link
         href={"/subscriptions/1"}
-        className="mt-4 rounded bg-primary p-4 text-white"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
       >
         Go to Subscription Details
-      </Link>
+      </Link> */}
     </SafeAreaView>
   );
 }
